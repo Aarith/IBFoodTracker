3b73bd36c35ae3e7b10063aad82f827bddabc64d
Ian Campbell Brothers <iab15a@lib-301-09-9118.local>
2019-02-15 08:13:25 -0600

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Ian Campbell Brothers on 2/15/19.
+//  Copyright © 2019 Ian Campbell Brothers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
