d38b64677071d533ba3a572c9046b026f041768e
Ian Campbell Brothers <iab15a@lib-301-09-9118.local>
2019-02-18 08:52:40 -0600

    Added the ability to rename the label via the text box.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // MARK: Properties
+    
+    @IBOutlet weak var MealName: UITextField!
+    @IBOutlet weak var MealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        MealName.delegate = self
     }
 
 
+    // MARK: UITextFieldDelegates
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        // Hide the Keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField){
+        MealNameLabel.text = textField.text
+    }
+    
+    
+        // MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        MealNameLabel.text = "Default Text"
+    }
+    
 }
 
